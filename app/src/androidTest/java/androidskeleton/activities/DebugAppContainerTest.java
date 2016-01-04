29f21d94d73240aa42e4c5997ceb55729b74e70d
Poeschl <poeschl_markus@web.de>
2016-01-04 12:52:10 +0100

    Added ConditionalIgnoreRule

--- app/src/androidTest/java/androidskeleton/activities/DebugAppContainerTest.java
+++ app/src/androidTest/java/androidskeleton/activities/DebugAppContainerTest.java
@@ -5,12 +5,13 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.Gravity;
 
-import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import androidskeleton.utils.ConditionalIgnoreRule;
+import androidskeleton.utils.ConditionalIgnoreRule.ConditionalIgnore;
 import apps.androidskeleton.BuildConfig;
 import apps.androidskeleton.R;
 
@@ -26,21 +27,22 @@ public class DebugAppContainerTest {
     @Rule
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(
             MainActivity.class);
+    @Rule
+    public ConditionalIgnoreRule ignoreRule = new ConditionalIgnoreRule();
 
     @Before
     public void setUp() throws Exception {
-        //Todo: Better way of conditionally ignore tests
-        Assume.assumeTrue("Skip because the debug drawer is not included in release builds", BuildConfig.DEBUG);
-
         openDebugDrawer();
     }
 
     @Test
+    @ConditionalIgnore(condition = NotInDebug.class)
     public void testDebugDrawerExists() {
         onView(withText(R.string.development_settings)).check(matches(isDisplayed()));
     }
 
     @Test
+    @ConditionalIgnore(condition = NotInDebug.class)
     public void testBuildSectionSetup() throws Exception {
         onView(withId(R.id.debug_build_name)).check(matches(withText(BuildConfig.VERSION_NAME)));
         onView(withId(R.id.debug_build_code)).check(matches(withText(Integer.toString(BuildConfig.VERSION_CODE))));
@@ -48,11 +50,13 @@ public class DebugAppContainerTest {
     }
 
     @Test
+    @ConditionalIgnore(condition = NotInDebug.class)
     public void testDeviceInfoAvailable() throws Exception {
         onView(withText("Device Information")).check(matches(isDisplayed()));
     }
 
     @Test
+    @ConditionalIgnore(condition = NotInDebug.class)
     public void testScalpelInfoAvailable() throws Exception {
         onView(withId(R.id.debug_ui_scalpel)).check(matches(isDisplayed()));
         onView(withId(R.id.debug_ui_scalpel_wireframe)).check(matches(isDisplayed()));
@@ -61,4 +65,11 @@ public class DebugAppContainerTest {
     private void openDebugDrawer() {
         onView(withId(R.id.debug_activity_layout)).perform(DrawerActions.open(Gravity.END));
     }
+
+    public class NotInDebug implements ConditionalIgnoreRule.IgnoreCondition {
+        public boolean isSatisfied() {
+            return !BuildConfig.DEBUG;
+        }
+    }
+
 }
