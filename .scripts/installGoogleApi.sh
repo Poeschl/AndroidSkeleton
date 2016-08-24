fb379d93a68b70da10e24b88aa4886cc226bf383
Poeschl <poeschl_markus@web.de>
2016-08-24 15:15:39 +0200

    wrong interpreter

--- .scripts/installGoogleApi.sh
+++ .scripts/installGoogleApi.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/bin/sh
 mkdir googleapi
 cd googleapi
 wget -nc https://github.com/opengapps/arm/releases/download/20160824/open_gapps-arm-7.0-pico-20160824.zip
@@ -8,3 +8,5 @@ adb push GmsCore.apk /system/priv-app/
 adb push GoogleServicesFramework.apk /system/priv-app/
 adb push GoogleLoginService.apk /system/priv-app/
 adb push Phonesky.apk /system/priv-app/
+adb shell stop
+adb shell start
