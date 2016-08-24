c300ac844855461cf3c11925c57fe6c3dd9cb834
Poeschl <poeschl_markus@web.de>
2016-08-24 14:49:42 +0200

    Manual google api install test

---
+++ .scripts/installGoogleApi.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/sh
+mkdir googleapi
+cd googleapi
+wget -nc https://github.com/opengapps/arm/releases/download/20160824/open_gapps-arm-7.0-pico-20160824.zip
+unzip open_gapps-arm-7.0-pico-20160824.zip
+adb remount
+adb push GmsCore.apk /system/priv-app/
+adb push GoogleServicesFramework.apk /system/priv-app/
+adb push GoogleLoginService.apk /system/priv-app/
+adb push Phonesky.apk /system/priv-app/
