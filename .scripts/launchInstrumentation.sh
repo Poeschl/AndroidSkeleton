a4d20702d99e174ee596a01c854aeb0da85ad6c0
Poeschl <poeschl_markus@web.de>
2016-08-31 11:33:00 +0200

    Now test results are also considered

--- .scripts/launchInstrumentation.sh
+++ .scripts/launchInstrumentation.sh
@@ -3,6 +3,7 @@ component=$1/$2
 instrumentationResult=`adb shell "am instrument -w $component ; printf \"$?\""`
 printf "$instrumentationResult\n"
 exitCode=$(printf "$instrumentationResult" | tail -1)
-if [ $exitCode != "0" ]; then
+failures=`printf "$instrumentationResult" | grep "Failures: 0"`
+if [ ${exitCode} != "0" -o -z "${failures}" ]; then
     exit 1
 fi
